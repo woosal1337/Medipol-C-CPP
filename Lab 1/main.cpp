b76dbcad02431551cfe35ceb902377566138af72
Vusal Ismayilov <69302858+woosal1337@users.noreply.github.com>
2020-10-21 23:43:32 +0300

    2.0

--- Lab 1/main.cpp
+++ Lab 1/main.cpp
@@ -1,6 +1,20 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
 #include <iostream>
+#include <algorithm>
+
+using namespace std;
+
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    int i = 0;
+    int total = 0;
+
+    while (cin >> i) {
+        total += i;
+    }
+    cout << total << endl;
+
     return 0;
 }
