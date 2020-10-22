d48e6c30413c8ef3135bbdc508ce357d9d4d07fd
Vusal Ismayilov <69302858+woosal1337@users.noreply.github.com>
2020-10-22 10:47:22 +0300

    4.0

--- HackerRank/Pointer/main.cpp
+++ HackerRank/Pointer/main.cpp
@@ -1,13 +1,19 @@
-#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
 
-void increment(int *v) {
-    (*v)++;
+void update(int *a,int *b) {
+    // Complete this function
+    *a = *a + *b;
+    *b =abs(*a - *b -*b);
 }
 
 int main() {
-    int a;
-    scanf("%d", &a);
-    increment(&a);
-    printf("%d", a);
+    int a, b;
+    int *pa = &a, *pb = &b;
+
+    scanf("%d %d", &a, &b);
+    update(pa, pb);
+    printf("%d\n%d", a, b);
+
     return 0;
 }
