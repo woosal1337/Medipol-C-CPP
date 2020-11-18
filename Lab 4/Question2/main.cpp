8d26c13d838d74b0a826ef62865732fe1b2eaf2e
Vusal Ismayilov <woosal@protonmail.com>
2020-11-18 19:53:02 +0300

    4.0

---
+++ Lab 4/Question2/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    // Unfortunately sizeOf() function does not work in my IDE, so I had to declare and integer,
+    // and count every iteration till 250, so if needed to stop it.
+
+    bool isRun = true;
+
+    int userList[250];
+    int userIntInput;
+    int totalUserInput = 0;
+    while (isRun) {
+        cout << "Enter a value between 100 & 500: ";
+        cin >> userIntInput;
+
+        
+
+
+    }
+
+
+
+    return 0;
+}
