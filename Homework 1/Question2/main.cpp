5bd21755d28570c51379c5ba4081a966169c6e03
Vusal Ismayilov <woosal@protonmail.com>
2020-11-21 21:00:21 +0300

    1.0

--- Homework 1/Question2/main.cpp
+++ Homework 1/Question2/main.cpp
@@ -6,16 +6,66 @@
 //
 // Student ID: 64190012
 //
-// Purpose: Given code below is going to take a user input in integer
-// format, and then based on that year will create the calendar and
-// show it as an output.
+// Purpose: Given an unknown / random number of different arrays, each
+// holding also random values ranging from 1 to 100 and randomly generated
+// all of the biggest values of each array is being also saved and sorted
+// and shown at the end
 //
 //---------------------------------------------------------------------
 
 #include <iostream>
+#include <time.h>
+#include <algorithm>
+
 using namespace std;
 
+// A function to find the greatest value of the randomly generated arrays.
+int theBiggest(int arr[]) {
+    int i, n;
+    n = 10;
+
+    // Loop to store largest number to arr[0]
+    for (i = 1; i < n; ++i) {
+        if (arr[0] < arr[i])
+            arr[0] = arr[i];
+    }
+    return arr[0];
+}
+
+
 int main() {
-    cout << "Hello, World!" << endl;
-    return 0;
+
+    int i, n, j, biggestArrValue, randomIteration, a[10], finalArr[100];
+    time_t t;
+
+    // Initializing a custom seed depending on the current time.
+    srand((unsigned) time(&t));
+    randomIteration = rand() % 100;
+
+    for (j = 0; j < randomIteration; j++) {
+        // Assigning the new arrays, and random values to them
+        for (i = 0; i < 10; i++) {
+            a[i] = rand() % 100;
+        }
+
+        // Finding the greatest value of the randomly generated array
+        // and assigning it to the biggestArrValue
+        biggestArrValue = theBiggest(a);
+
+        // To keep the progress, all the greatest values of the randomly generated
+        // lists are being appended to the array named finalArr
+        finalArr[j] = biggestArrValue;
+    }
+
+
+    // Sorting out the final array
+    sort(begin(finalArr), end(finalArr), greater<>());
+
+
+    // Printing out the final/sorted result below
+    for (int x = 0; x < randomIteration; x++) {
+        cout << finalArr[x] << "\n";
+    }
+
+    return (0);
 }
